1bdc7d3767a77de1e8b5c98c2e2d69df621e447d
smathur93 <shreymathur93@gmail.com>
2025-06-21 23:46:40 +0530

    Adding old schema from hero academy v2 for entity and dao

--- src/test/java/com/shrek/AdventurerAcademyV1Service/AdventurerAcademyV1ServiceApplicationTests.java
+++ src/test/java/com/shrek/AdventurerAcademyV1Service/AdventurerAcademyV1ServiceApplicationTests.java
@@ -1,9 +1,12 @@
-package com.shrek.AdventurerAcademyService;
+package com.shrek.AdventurerAcademyV1Service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@ComponentScan(basePackages = "com.shrek.AdventurerAcademyV1Service")
 class AdventurerAcademyV1ServiceApplicationTests {
 
 	@Test
